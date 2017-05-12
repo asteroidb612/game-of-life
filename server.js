adfaea027d9e9859ffa90fd86a54d5bab716828e
Drew Lazzeri <dlazzeri1@gmail.com>
2017-05-12 00:41:04 -0700

    Found heroku local

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ io.on('connection', function(socket) {
   });
 });
 
-app.use(express.static('assets'));
+app.use(express.static(__dirname + '/assets'));
 app.listen(3000);
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port')); 
