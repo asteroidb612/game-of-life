9a84d54a8b4ec4a9e75f609be99cfecbed0b1309
Drew Lazzeri <dlazzeri1@gmail.com>
2017-05-12 00:17:52 -0700

    Syntax error, Should have run locally

--- server.js
+++ server.js
@@ -50,7 +50,8 @@ io.on('connection', function(socket) {
 app.use(express.static('assets'));
 app.listen(3000);
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+  console.log('Node app is running on port', app.get('port')); 
+});
 
 var id = gameloop.setGameLoop(function(delta) {
   if (!game.running) {
